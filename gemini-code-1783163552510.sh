7a9017429cc6ce13ef3a04ef22c2ba99145c38d4
Dineshparamata <140137490+Dineshparamata@users.noreply.github.com>
2026-07-04 16:47:14 +0530

    Add files via upload

---
+++ gemini-code-1783163552510.sh
@@ -0,0 +1,6 @@
+git init
+git add .
+git commit -m "Initial commit of Live Encyclopedia"
+git branch -M main
+git remote add origin https://github.com/Dineshparamata/realworld-encyclopedia.git
+git push -u origin main
